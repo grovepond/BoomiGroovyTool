d88ca3b919c7d613300b1297159d0cbf3caea925
grovepond <grovepond@protonmail.com>
2024-05-07 13:21:21 +0200

    Added testfiles and made some fixes

--- src/com/boomi/execution/Logger.java
+++ src/com/boomi/execution/Logger.java
@@ -1,10 +1,10 @@
 package com.boomi.execution;
 
 public class Logger {
-    public void info(String message) { System.out.println(message); }
-    public void warning(String message) { System.out.println(message); }
-    public void severe(String message) { System.out.println(message); }
-    public void fine(String message) { System.out.println(message); }
-    public void finer(String message) { System.out.println(message); }
-    public void finest(String message) { System.out.println(message); }
+    public void info(String text) { System.out.println("INFO: " + text); }
+    public void warning(String text) { System.out.println("WARNING: " + text); }
+    public void severe(String text) { System.out.println("SEVERE: " + text); }
+    public void fine(String text) { System.out.println("FINE: " + text); }
+    public void finer(String text) { System.out.println("FINER: " + text); }
+    public void finest(String text) { System.out.println("FINEST: " + text); }
 }
