2eb4e5f8d3fa5e9c7bc192885c9680e5585ae166
grovepond <grovepond@protonmail.com>
2024-05-08 14:35:05 +0200

    Moved some files around

--- src/Test.java
+++ src/Test.java
@@ -10,10 +10,10 @@ public class Test {
         dataContext.attachFiles(inputFilePath);
         ExecutionUtil ExecutionUtil = new ExecutionUtil();
 
-        ExecutionUtil.setExecutionProperty("LastRunDate", new java.util.Date().toString());
+        ExecutionUtil.setExecutionProperty("com.boomi.execution.lastrun", new java.util.Date().toString());
 
         // Set Process property
-        ExecutionUtil.setProcessProperty("e664d4fc-2e22-4898-ba05-1a898409a813", "sample_property", inputFilePath);
+        ExecutionUtil.setProcessProperty("e664d4fc-2e22-4898-ba05-1a898409a813", "sample_property", "sample_property-value");
 
         // Set Dynamic Process Property
         ExecutionUtil.setDynamicProcessProperty("DPP_sample", "DPP_sample-value", false);
@@ -27,8 +27,7 @@ public class Test {
             dataContext.addDynamicDocumentPropertyValues(i, "DDP-sample", inputFilePath + "DDP_sample-value-" + i);
 
         Logger logger = ExecutionUtil.getBaseLogger();
-        logger.info(ExecutionUtil.getDynamicProcessProperty("DPP_sample"));
-        logger.fine(ExecutionUtil.getExecutionProperty("LastRunDate"));
+
 
         for (int i = 0; i < dataContext.getDataCount(); i++) {
             InputStream is = dataContext.getStream(i);
@@ -37,6 +36,8 @@ public class Test {
             props.setProperty("document.dynamic.userdefined.filecontent", is.toString());
 
             logger.info("DDP: " + props.getProperty("document.dynamic.userdefined.DDP-sample"));
+            logger.info("PP: " + ExecutionUtil.getProcessProperty("e664d4fc-2e22-4898-ba05-1a898409a813", "sample_property"));
+            logger.info("EP: " + ExecutionUtil.getExecutionProperty("com.boomi.execution.lastrun"));
             dataContext.storeStream(is, props);
         }
 
